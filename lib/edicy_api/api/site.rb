62f2d18a8b363719ede7b729bb29bff7d0005f89
Tanel Jakobsoo <tanel.jakobsoo@gmail.com>
2014-01-29 11:02:36 +0200

    Updated Site API

--- lib/edicy_api/api/site.rb
+++ lib/edicy_api/api/site.rb
@@ -2,10 +2,29 @@ module Edicy
   class API
     module Site
 
-      def site
-        get 'site'
+      # Get site attributes
+      def site(params = {})
+        get 'site', {query: params}
       end
 
+      # Update a site
+      def update_site(data)
+        put "site", data
+      end
+
+      # Update/create a key in site data field
+      #
+      # @params id [String] key in data field
+      def update_site_data(id, data)
+        put "site/data/#{id}", {value: data}
+      end
+
+      # Delete a key from site data field
+      #
+      # @params id [String] key in data field
+      def delete_site_data(id)
+        delete "site/data/#{id}"
+      end
     end
   end
 end
---
+++ spec/edicy_api/api/site_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Edicy::API::Site do
+
+  let(:client) { edicy_client }
+
+  describe '#site' do
+    before do
+      request_fixture(:get, 'site', fixture: 'site/site')
+    end
+
+    it 'returns a single site' do
+      expect(client.site.search_enabled).to eq(false)
+    end
+
+    it 'returns a site with the same meta_keywords as in the request' do
+      expect(client.site.meta_keywords).to eq('my key words')
+    end
+  end
+
+  describe '#update_site' do
+
+    before do
+      request_fixture(:put, 'site', request: {body: {meta_keywords: 'new keys'}}, response: {body: '{"meta_keywords": "new keys"}'})
+    end
+
+    it 'responds with settings' do
+      expect(client.update_site(meta_keywords: 'new keys').meta_keywords).to eq('new keys')
+    end
+  end
+
+  describe '#update_site_data' do
+
+    before do
+      request_fixture(:put, 'site/data/my_key', request: {body: {value: 'New key'}}, response: {body: '{"data": {"my_key": "New key"}}'})
+    end
+
+    it 'responds with new title' do
+      expect(client.update_site_data('my_key', 'New key').data.my_key).to eq('New key')
+    end
+  end
+end
