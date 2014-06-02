46d092d44eda6325d43f70c10a2c132c200be1c2
Tanel Jakobsoo <tanel.jakobsoo@gmail.com>
2014-06-02 11:26:45 +0300

    Added Content partials API

---
+++ lib/edicy_api/api/content_partials.rb
@@ -0,0 +1,21 @@
+module Edicy
+  class API
+    module ContentPartials
+
+      # List content partials contents
+      def content_partials(params = {})
+        get 'content_partials', {query: params}
+      end
+
+      # Get a single content partial content
+      def content_partial(id, params = {})
+        get "content_partials/#{id}", {query: params}
+      end
+
+      # Update a content partial content
+      def update_content_partial(id, data)
+        put "content_partials/#{id}", data
+      end
+    end
+  end
+end
